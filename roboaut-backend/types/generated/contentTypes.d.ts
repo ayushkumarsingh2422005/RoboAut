ae133b51eea5b4f2bf9cd2381e2dc3056e98613c
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2025-08-27 02:52:16 +0530

    update on phone no

--- roboaut-backend/types/generated/contentTypes.d.ts
+++ roboaut-backend/types/generated/contentTypes.d.ts
@@ -451,7 +451,7 @@ export interface ApiEventEvent extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    Date: Schema.Attribute.DateTime & Schema.Attribute.Required;
+    Date: Schema.Attribute.Date & Schema.Attribute.Required;
     Description: Schema.Attribute.Text & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::event.event'> &
